74856e24d47d0735268becb057d6a27091b46c7d
Suman Farhan <51996425+SumanFarhan@users.noreply.github.com>
2022-04-18 12:14:15 +0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,41 @@
+// window.onload=function(){
+//     show (0);
+//     Start();
+// }
+
+const questions=
+    [{
+        question:"What is the answer of 8+5?",
+        answers:["2","13","11","10","9"],
+        correctanswer:"13"
+        },
+    {
+        question:"What is the answer of 2*5?",
+        answers:["2","13","11","10","9"],
+        correctanswer:"10"
+    },
+    {
+        question:"What is the answer of 3-10?",
+        answers:["2","-7","7","-10","-9"],
+        correctanswer:"-7"
+    },
+    {
+        question:"What is the answer of 6*10?",
+        answers:["2","13","60","10","61"],
+        correctanswer:"60"
+    },
+    {
+        question:"What is the answer of 2/10?",
+        answers:["2","0.4","0.3","0.6","0.2"],
+        correctanswer:"0.2"
+    }]
+
+
+let ques_count=0;
+let wrongAnswer=0;
+let score=0;
+
+function next(){
+            let userAnswer=document.querySelectorAll("li.active").innerHTML;
+            if (userAnswer == )
+}
